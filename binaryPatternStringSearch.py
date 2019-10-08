8bc268f9d241b74eb2bc6011a04ed321bb512030
max <maximohinojosa24@yahoo.com>
2019-10-08 13:09:18 -0700

    sorted_squares

--- binaryPatternStringSearch.py
+++ binaryPatternStringSearch.py
@@ -15,9 +15,7 @@ def isValid(window, pattern):
     v = {'a': '0', 'e': '0', 'i': '0', 'o': '0', 'u': '0', 'y': '0'}
 
     for i in range(0 , len(window)):
-        # print('window: ', window)
         letter = window[i]
-
         if len(window) != len(pattern):
             return False
         elif pattern[i] == '0':
@@ -28,23 +26,19 @@ def isValid(window, pattern):
 
     return True
 
-p = '010'
+p = '0'
 s = 'amazing'
-
-print('Test 1: correct: 2, result = {}'.format(binaryPatternSearch(p, s)))
+print('Test 1: correct: 3, result = {}'.format(binaryPatternSearch(p, s)))
 
 p = '0'
 s = 'elegant'
-
 print('Test 2: correct: 3, result = {}'.format(binaryPatternSearch(p, s)))
 
 
 p = '101'
 s = 'riding'
-
 print('Test 3: correct: 2, result = {}'.format(binaryPatternSearch(p, s)))
 
 p = '1011'
 s = 'herbivorous'
-
 print('Test 4: correct: 1, result = {}'.format(binaryPatternSearch(p, s)))
