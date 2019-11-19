a31b955df8547fabcffa783455cc996f88efafb7
max <maximohinojosa24@yahoo.com>
2019-11-18 18:14:59 -0800

    [solved] add strings problem

---
+++ array_Pair_Sum.py
@@ -0,0 +1,23 @@
+
+
+
+
+'''
+561. Array Partition I
+Input: [1,4,3,2]
+
+Output: 4
+Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+'''
+# def arrayPairSum(nums: [int]) -> int:
+#
+#     for i in range(0, len(nums)):
+
+
+
+def do():
+    col = ['Freshman', 'Sophmore', 'Junior', 'Senior']
+    return list(enumerate(col, 2019))
+
+
+print(do())
---
+++ sum_of_str.py
@@ -0,0 +1,22 @@
+
+
+def convertToNum(numStr: str) -> int:
+    result = 0
+
+    value = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+    for i in numStr:
+        result = 10 * result + value[i]
+    return result
+
+def addStrings(num1: str, num2: str) -> str:
+    numstr = '{}'.format((convertToNum(num1) + convertToNum(num2)))
+    return numstr
+
+
+
+if __name__ == '__main__':
+    num1 = '12'
+    num2 = '12'
+    result = addStrings(num1, num2)
+    print("result: {}, expected: {}".format(result, '24'))
